b3e97c26f02b0c174e774e76d9de3eed388d5c5e
Aleksey <aleksey.andropovw@gmail.com>
2024-01-30 20:49:53 +0300

    added connection to epic (EPIC ID)

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -1,7 +1,14 @@
 package model;
 
 public class Subtask extends Task {
-    public Subtask(String name, String description) {
+    private final int EPIC_ID;
+
+    public Subtask(String name, String description, Epic epic) {
         super(name, description);
+        this.EPIC_ID = epic.getId();
+    }
+
+    public int getEPIC_ID() {
+        return EPIC_ID;
     }
 }
