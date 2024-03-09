e14183abc09c419804522d2026f21f88b4e0156f
AlekseyEsc <aleksey.andropovw@gmail.com>
2024-03-10 02:33:23 +0300

    fix code style in Subtask.java

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -1,21 +1,21 @@
 package model;
 
 public class Subtask extends Task {
-    private final int EPIC_ID;
+    private final int epicId;
 
     public Subtask(String name, String description, Epic epic) {
         super(name, description);
-        this.EPIC_ID = epic.getId();
+        this.epicId = epic.getId();
     }
 
     @Override
     public void setId(int id) {
-        if (id != EPIC_ID) {
+        if (id != epicId) {
             super.setId(id);
         }
     }
 
     public int getEpicId() {
-        return EPIC_ID;
+        return epicId;
     }
 }
