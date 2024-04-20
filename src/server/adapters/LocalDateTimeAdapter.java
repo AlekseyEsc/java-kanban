70bb6eeea80951a5020684ade7f002f4b62a8e05
AlekseyEsc <aleksey.andropovw@gmail.com>
2024-04-20 23:40:26 +0300

    change formatter date

--- src/server/adapters/LocalDateTimeAdapter.java
+++ src/server/adapters/LocalDateTimeAdapter.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy hh:mm:ss");
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
 
     @Override
     public void write(JsonWriter jsonWriter, LocalDateTime localDateTime) throws IOException {
